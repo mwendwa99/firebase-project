1acd7aaa11ebd66041204f330b7ad223c30998e5
Brian Mwendwa <brianmwendwa1999@gmail.com>
2021-07-13 19:53:37 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from './firebase';
 
-function App() {
+const App = () => {
+
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAcc, setHasAcc] = useState(false);
+
+  // clear inputs
+  const clearInput = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  // clear errors
+  const clearError = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  // handle login
+  const handleLogin = () => {
+    clearError();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case 'auth/invalid-email':
+          case 'auth/user-disabled':
+          case 'auth/user-not-found':
+            setEmailError(err.message);
+            break;
+          case 'auth/wrong-password':
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  };
+
+  // handle signup
+  const handleSignup = () => {
+    clearError();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case 'auth/email-already-in-use':
+          case 'auth/invalid-email':
+            setEmailError(err.message);
+            break;
+          case 'auth/weak-password':
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  };
+
+  // handle logout
+  const handleLogout = () => {
+    firebase.auth().signOut();
+  };
+
+  // listerner to check whether user signup or login
+  const authListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInput();
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    })
+  }
+
+  useEffect(() => {
+    authListener();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world!
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
